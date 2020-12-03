142c3d8ec2def6d1598343f6f59d8263e1a900a3
christina <christina@christinas-iMac.local>
2020-12-03 17:50:09 -0600

    create url_to_list function

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 import html5lib
 import requests
 
+def url_to_list(url):
+    # URL = "https://mirror1.malwaredomains.com/files/justdomains"
+    r = requests.get(url)
+    block_list = BeautifulSoup(r.content, 'html5lib') 
 
-URL = "https://mirror1.malwaredomains.com/files/justdomains"
-r = requests.get(URL)
-block_list = BeautifulSoup(r.content, 'html5lib') 
+    with open('Ultimate_block_list.txt', 'w') as file:
+        file.write(block_list.prettify())
 
-block_list_name = 'Ultimate_block_list.txt'
-with open(block_list_name, 'w') as f:
-    f.write(block_list.prettify())
+if __name__ == '__main__':
+    url_to_list(https://mirror1.malwaredomains.com/files/justdomains)
