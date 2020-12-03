555a7bbe05fab2b7eb4ee1ce797e9a0013f937c0
christina <christina@christinas-iMac.local>
2020-12-03 16:18:46 -0600

    creating txt for blocklist

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from bs4 import BeautifulSoup
 import html5lib
 import requests
+
+
+URL = "https://mirror1.malwaredomains.com/files/justdomains"
+r = requests.get(URL)
+block_list = BeautifulSoup(r.content, 'html5lib') 
+
+block_list_name = 'Ultimate_block_list.txt'
+with open(block_list_name, 'w') as f:
+    f.write(block_list.prettify())
